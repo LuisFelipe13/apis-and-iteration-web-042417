a5c693d39586ffb4dba89186bf1ad66754ca4307
Luis del Cueto <luisdelcueto@live.com>
2017-04-26 12:05:47 -0400

    finished accessing films

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,38 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# iterate over the character hash to find the collection of `films` for the given
+#   `character`
+# collect those film API urls, make a web request to each URL to get the info
+#  for that film
+# return value of this method should be collection of info about each film.
+#  i.e. an array of hashes in which each hash reps a given film
+# this collection will be the argument given to `parse_character_movies`
+#  and that method will do some nice presentation stuff: puts out a list
+#  of movies by title. play around with puts out other info about a given film.
+
 def get_character_movies_from_api(character)
   #make the web request
+  movies = []
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  character_hash.each do |key, value|
+    if key == "results"
+      value.each do |keys|
+        keys.each do |stats, info|
+          if info == character
+            movies << character_hash["results"][0]["films"]
+          end
+        end
+      end
+    end
+  end
+  puts movies
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  
 end
 
 def show_character_movies(character)
@@ -31,3 +45,4 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+binding.pry
